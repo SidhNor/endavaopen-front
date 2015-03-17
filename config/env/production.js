d644b2625896134e9e3aa7582cc4c4949ab676a6
Gleb Godonoga <gleb.godonoga@gmail.com>
2015-03-17 12:48:42 +0200

    Update

--- config/env/production.js
+++ config/env/production.js
@@ -33,8 +33,6 @@ module.exports = {
     adapter: 'mongo',
     host: process.env.OPENSHIFT_MONGODB_DB_HOST,
     port: process.env.OPENSHIFT_MONGODB_DB_PORT,
-    user: process.env.OPENSHIFT_MONGODB_DB_USERNAME,
-    password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD,
     db: 'sails',
     collection: 'sessions'
   }
