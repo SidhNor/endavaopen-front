63c2805e9ed5de52b18517b4ea9bbf1bd31ae127
Gleb Godonoga <gleb.godonoga@gmail.com>
2015-03-17 13:25:53 +0200

    Added db name

--- config/connections.js
+++ config/connections.js
@@ -45,6 +45,8 @@ module.exports.connections = {
     host: process.env.OPENSHIFT_MONGODB_DB_HOST,
     port: process.env.OPENSHIFT_MONGODB_DB_PORT,
     user: process.env.OPENSHIFT_MONGODB_DB_USERNAME,
-    password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD
+    password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD,
+    database: 'open'
+
   }
 };
