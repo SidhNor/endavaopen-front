89071e73d1d89796816f449a285bf6a5edecf8ba
Gleb Godonoga <gleb.godonoga@gmail.com>
2015-03-17 16:09:27 +0200

    a

--- config/env/production.js
+++ config/env/production.js
@@ -33,7 +33,7 @@ module.exports = {
     adapter: 'mongo',
     host: process.env.OPENSHIFT_MONGODB_DB_HOST,
     port: process.env.OPENSHIFT_MONGODB_DB_PORT,
-    user: process.env.OPENSHIFT_MONGODB_DB_USERNAME,
+    username: process.env.OPENSHIFT_MONGODB_DB_USERNAME,
     password: process.env.OPENSHIFT_MONGODB_DB_PASSWORD,
     db: 'open',
     collection: 'sessions'
