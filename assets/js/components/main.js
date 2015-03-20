797164fb7b9e35012454df096ba935afd9b8e12a
Gleb Godonoga <gleb.godonoga@gmail.com>
2015-03-19 22:44:07 -0700

    Added sample menu

--- assets/js/components/main.js
+++ assets/js/components/main.js
@@ -3,23 +3,43 @@ var Router = require('react-router');
 var RouteHandler = Router.RouteHandler;
 var mui = require('material-ui');
 var AppBar = mui.AppBar;
+var Toolbar = mui.Toolbar;
+var ToolbarGroup = mui.ToolbarGroup;
+var Tabs = mui.Tabs;
+var Tab = mui.Tab;
+var IconButton = mui.IconButton;
+var ActionGrade = mui.Action
 var AppCanvas = mui.AppCanvas;
+var RaisedButton= mui.RaisedButton;
+var NavigationMenu = mui.Icons.NavigationMenu;
 
 var Main = React.createClass({
 
   mixins: [Router.State],
 
-  render: function() {
+  render: function () {
     var title = 'Endava Open';
 
     return (
       <AppCanvas predefinedLayout={1}>
+        <Toolbar className="open-menu">
+          <ToolbarGroup key={0} float="left">
+            <IconButton touch={true}>
+              <NavigationMenu/>
+            </IconButton>
+          </ToolbarGroup>
+          <ToolbarGroup key={1} float="right">
+            <Tabs tabWidth="150">
+              <Tab label="About"
+                route="home"
+                onActive={this._onActive} />
+              <Tab label="Schedule" route="home"/>
+              <Tab label="Rules" route="home"/>
+              <Tab label="Player" route="home"/>
+            </Tabs>
+          </ToolbarGroup>
 
-        <AppBar
-          className="mui-dark-theme"
-          title={title}
-          zDepth={0}>
-        </AppBar>
+        </Toolbar>
 
         <RouteHandler />
 
@@ -31,6 +51,10 @@ var Main = React.createClass({
 
       </AppCanvas>
     );
+  },
+
+  _onActive: function (tab) {
+    this.transitionTo(tab.props.route);
   }
 
 });
