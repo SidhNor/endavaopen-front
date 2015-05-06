35a410da9fcfb1c28ec7c766bdc4e1f7251368c3
Gleb Godonoga <gleb.godonoga@gmail.com>
2015-05-06 18:41:44 +0300

    Fixed footer style

---
+++ assets/js/components/controls/match-card.js
@@ -0,0 +1,13 @@
+var React = require('react');
+var mui = require('material-ui');
+var Paper = mui.Paper;
+
+var actions    = require('../../actions/actions');
+var MatchCard = React.createClass({
+
+  render: function () {
+   return '';
+  }
+});
+
+module.exports = MatchCard;
