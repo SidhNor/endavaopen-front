f4bc71461b607b539330a0ad4f1cb8655034c047
Gleb Godonoga <gleb.godonoga@gmail.com>
2015-05-13 17:05:20 +0300

    Updated prod configs

--- config/env/production.js
+++ config/env/production.js
@@ -25,7 +25,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: process.env.OPENSHIFT_NODEJS_PORT || 80,
+  port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
 
   host: process.env.OPENSHIFT_NODEJS_IP,
 
